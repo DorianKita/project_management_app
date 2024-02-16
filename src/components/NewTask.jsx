2dc0402a7aa9c5e600ab3c0ae5d547dacfbae0d0
Dorian Kita <dorian.kita90@gmail.com>
2024-02-16 12:09:26 +0100

    created add task functionality i  Task component

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,8 +1,31 @@
-export default function NewTask() {
+import { useState } from "react";
+
+export default function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState();
+
+  function handleChange(e) {
+    setEnteredTask(e.target.value);
+  }
+
+  function handleClick() {
+    setEnteredTask("");
+    onAdd(enteredTask);
+  }
+
   return (
     <div className="flex item-center gap-4">
-      <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-      <button className="text-stone-700 hover:text-stone-950">Add Task</button>
+      <input
+        onChange={handleChange}
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        value={enteredTask}
+      />
+      <button
+        className="text-stone-700 hover:text-stone-950"
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
     </div>
   );
 }
